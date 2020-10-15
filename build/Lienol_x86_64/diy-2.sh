0422ac490df8f8da153e93ca4b6eeeb8532c2aa5
wx611975 <57552340+wx611975@users.noreply.github.com>
2020-10-15 13:11:51 +0800

    Update diy-2.sh

--- build/Lienol_x86_64/diy-2.sh
+++ build/Lienol_x86_64/diy-2.sh
@@ -29,7 +29,7 @@ sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' package/lean/luci-app-sfe/p
 sed -i 's/"KMS 服务器"/"KMS激活"/g' package/lean/luci-app-vlmcsd/po/zh-cn/vlmcsd.zh-cn.po
 #sed -i 's/"USB 打印服务器"/"打印服务"/g' package/lean/luci-app-usb-printer/po/zh-cn/usb-printer.po
 #sed -i 's/"网络存储"/"存储"/g' package/lean/luci-app-usb-printer/po/zh-cn/usb-printer.po
-sed -i 's/TTYD 终端/终端命令/g' feeds/luci/transplant/luci-app-ttyd/po/zh-cn/terminal.po
+#sed -i 's/TTYD 终端/终端命令/g' feeds/luci/transplant/luci-app-ttyd/po/zh-cn/terminal.po
 sed -i 's/"Web 管理"/"管理"/g' package/lean/luci-app-webadmin/po/zh-cn/webadmin.po
 sed -i 's/"管理权"/"密码"/g' feeds/luci/modules/luci-base/po/zh-cn/base.po
 #sed -i 's/"带宽监控"/"监视"/g' feeds/luci/applications/luci-app-nlbwmon/po/zh-cn/nlbwmon.po
